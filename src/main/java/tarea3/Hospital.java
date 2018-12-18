01e4340541fc778e1d97843880778d7bbb779660
Paralizis <felipe.santanderf@alumnos.usm.cl>
2018-12-18 18:49:44 -0400

    asd

--- src/main/java/tarea3/Hospital.java
+++ src/main/java/tarea3/Hospital.java
@@ -508,7 +508,12 @@ class BullyClient implements Runnable {
 			channels.add(ManagedChannelBuilder.forAddress(host, port).usePlaintext().build());
 			asyncStub = BullyGrpc.newStub(channels.get(channels.size()-1)); // TODO 1 stup x channel ??
 			System.out.println("[anunciaCoord] Channel: " + dest);
-			TimeUnit.SECONDS.sleep(5);
+
+			try{
+				TimeUnit.SECONDS.sleep(5);
+			} catch (InterruptedException e){
+				System.out.println("InterruptedException shutdown");
+			}
 
 			asyncStub.anuncioCoordinacion(msg, new StreamObserver<OkMsg>() {
 				@Override
